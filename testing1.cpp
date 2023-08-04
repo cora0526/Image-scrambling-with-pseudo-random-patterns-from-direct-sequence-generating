db18d9a38322bc7184b01e3a84627446f04badb9
Cora <candy273321@gmail.com>
2023-08-04 15:52:23 +0800

    feat: 稍微改了一下lfsr的演算法 但沒有很成功還要確認錯誤在哪裡 按照現在的設定應該要產出的序列應該為 121(pass)、60、94、111、119、59、29

--- testing1.cpp
+++ testing1.cpp
@@ -5,7 +5,7 @@
 #include <time.h>
 #include <math.h>
 #include <unistd.h>
-//x^6 + x^1 + 1
+//x^7 + x^3 + 1
 //初始值:0b1110010=114
 #define SIZEX 10
 #define SIZEY 12
@@ -17,24 +17,27 @@
 
 void arraprint (char*);
 int stagedefine (void);
-int lfsr (int ,int);
+int lfsr (int,int,int*);
 void fillerr (char*,int);
 int main (void){
+    int settingmatrix[32]={7,3,0,0,0,0,0,0,0,0,
+                           0,0,0,0,0,0,0,0,0,0,
+                           0,0,0,0,0,0,0,0,0,0,0,0};
     char matrix [SIZEY][SIZEX]={0};
     int stage = stagedefine();
-    int a = lfsr(INITIAL,stage);
+    int a = lfsr(INITIAL,stage,&settingmatrix[0]);
     for (int i=0;i<ERRORSIZE;i++)
     {   
         if (a>=(SIZEX*SIZEY)){
             printf("i=%d %d pass\n",i,a);
-            a = lfsr(a,stage);
+            a = lfsr(a,stage,&settingmatrix[0]);
             i=i-1;
             continue;
         }
         else {
             fillerr(&matrix[0][0],a);
             printf("i=%d %d\n",i,a);
-            a = lfsr(a,stage);
+            a = lfsr(a,stage,&settingmatrix[0]);
         }
         
     }
@@ -48,29 +51,58 @@ void fillerr (char* mat,int s){
     return;
 }
 
-int lfsr (int intial, int s){
+int lfsr (int intial, int s, int* set){
+    //設定x^7 + x^3 + 1
+    int mat[32]={0};
+
     int i,i1,i2,i3,i4,i5,i6,i7,i8,i9,i10=0;
     i=1;//7
     if ((i&intial)==1)
     {
-        i1=1;   
+        mat[0]=1;   
     }
-    else{i1=0;}
-    i<<=4;//3 
-    //printf("i<<1=%d\n",i);
-    if ((i&intial)==16)
-    {
-        i2=1;
-    }
-    else{i2=0;}        
+    else{mat[0]=0;}
+
+    int c=0;
+    int c1=s;
+    int setstop=0;
+    int j=1;
     
-    i=i1^i2;
+    for (int t=0;t<32;t++){
+        c=set[t];
+        if (c==0)
+        {
+            break;
+        }
+        setstop+=1;
+        c1=c1-c;
+        i<<=c1;
+        if ((i&intial)==(pow(2,(s-c))))
+        {
+            mat[j]=1;
+        }
+        else{mat[j]=0;} 
+        j+=1;
+    }
+    // i<<=4;//3 
+    // //printf("i<<1=%d\n",i);
+    // if ((i&intial)==16)
+    // {
+    //     i2=1;
+    // }
+    // else{i2=0;}        
+    
+    // i=i1^i2;
+    for (int ii=1;ii<setstop;ii++){
+        mat[0]=mat[ii]^mat[0];
+    }
     i<<=(s-1);
     intial>>=1;
-    intial+=i;
+    // intial+=i;
+    intial+=mat[0];
     int tem = pow(2,s)-1;
     intial&=tem;
-    // printf("%d\n",intial);
+    // printf("intial:%d\n",intial);
     return intial;
 }
 
