171596881ea5fa91bc7f034a75d89252e108283b
Cora <candy273321@gmail.com>
2023-08-15 17:02:36 +0800

    feat: 打亂照片

---
+++ dislocated.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+import cv2
+
+path = "22m-seq.csv"
+df = pd.read_csv(path)
+df_col = df.columns
+print(df_col)
+i=0
+for x in df['1048577']:
+    print(x)
+    i+=1
+    if i == 20:
+        break
+
+img_path = "R.jpg"
+img = cv2.imread(img_path)
+print(type(img))
